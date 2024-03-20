6ef78c516599da16d49530272976559f13fefbcc
Marcelotsfiilho <marcelo.estudo.cefet@gmail.com>
2024-03-20 19:31:00 -0300

    Atualizacao-pagina-modalidades-inicio-pagina-eventos

--- src/pages/Eventos.js
+++ src/pages/Eventos.js
@@ -1,11 +1,55 @@
-
+import styles from './pages_styles/Eventos.module.css';
+import BannerEventos from '../images/BannerEventos.png';
 function Eventos() {
     return (
-      <div className="Eventos">
-        <h1>Eventos</h1>
-      </div>
-    );
-  }
+      //Background Inical da Página
+      <section className={styles.Content}>
+        <div className={styles.overlay}>
+          <h1 className={styles.title}>EVENTOS</h1>
+          <img src={BannerEventos} alt="Banner" className={styles.bg}/>
+        </div>
+        
+        {/*Texto sobre nossos eventos*/}
+        <div className={styles.NossosEventos}>
+          <h1>NOSSOS EVENTOS</h1>
+          <hr/>
+          <pre>
+            {"A Atlética Panterão realiza diversos eventos durante o ano, sejam eles festas ou até mesmo eventos intitucionais. \n"}
+            {"Nosso objetivo é criar uma maior integração entre os alunos e deixar cada vez melhor sua experiência universitária. \n"}
+            {"Aqui, apresentamos algum de nossos eventos realizados durante o ano."}
+          </pre>
+
+
+
+          {/* <h2>A Atlética Panterão realiza diversos eventos durante o ano, sejam eles festas ou até mesmo eventos intitucionais. \n
+              Nosso objetivo é criar uma maior integração entre os alunos e deixar cada vez melhor sua experiência universitária.
+              Aqui, apresentamos algum de nossos eventos realizados durante o ano.</h2> */}
+        </div>
+
+        {/*Divisão da tela ao meio por div*/}
+        <div className={styles.DivisaoTela}>
+          {/*Elemenos à esquerda da tela*/}
+          <div className={styles.TelaEsquerda}>
+            <div className={styles.TextoEvento}>
+              <h1>RECEPÇÃO DE CALOUROS</h1>
+              <hr/>
+              <h2>A Recepção de calouros é a festa mais tradicional da Atlética.
+                  Acontece no inicio de cada período e tem como objetivo receber os novos alunos das engenharias do CEFET - Leopoldina.
+                  É uma oportunidade incrivel para fazer novas amizades, beber bastante e quem sabe até encontrar seu crush. Assim, a recepção de calouros é a melhor forma de começar o período.
+              </h2>
+            </div>
+          </div>
+
+          {/*Elementos à direita da tela*/}
+          <div className={styles.TelaDireita}>
+              
+          </div>
+        </div>
+
+
+      </section>
+    )
+}
   
   export default Eventos;
   
--- src/pages/Modalidades.js
+++ src/pages/Modalidades.js
@@ -1,7 +1,81 @@
+import styles from './pages_styles/Modalidades.module.css';
+import garra from '../images/garra 1.png';
+
 function Modalidades() {
     return (
-      <section>
-        <h1>Modalidades</h1>
+
+      /*Texto nossas modalidades*/
+      <section className={styles.Content}>
+        <div className={styles.TextModalidades}>
+          <h1>NOSSAS MODALIDADES</h1>
+          <hr/>
+          <h2>O Panterão conta com 9 modalidades físicas e 6 modalidades online.<br/>
+              Os Atletas participam de diversos campeonatos no decorrer do ano letivo, dos quais temos boas memórias e títulos para recordar.</h2>      
+        </div>
+
+        <div className={styles.DivisaoTela}>
+        {/*Importação da imagem da garra*/}
+          <div className={styles.BlocoEsquerda}>
+            <div className={styles.ImgGarra}>
+              <img src={garra} alt="GarraPantera" className={styles.Garra}/>
+            </div>
+          </div>
+
+          {/*Lista de modalidades praticadas*/}
+          <div className={styles.BlocoDireita}>
+            <div className={styles.ListModalidadesEsquerda}>
+              <h1>ESPORTES</h1>
+              <hr/>              
+              <li> FUTSAL</li>
+              <li> TÊNIS DE MESA</li>
+              <li> SINUCA</li>
+              <li> PETECA</li>
+              <li> FUTVOLEI</li>
+              <li> NATAÇÃO</li>
+              <li> ESCALADA</li>
+              <li> FUTEBOL</li>
+              <li> BASQUETE</li>
+              <li> VÔLEI</li>
+            </div>
+
+            <div className={styles.ListModalidadesDireita}>
+              <h1>E-SPORTS</h1>
+              <hr/>
+              <li> LEAGUE OF LEGENDS</li>
+              <li> COUNTER-STRIKE 2</li>
+              <li> VALORANT</li>
+              <li> OVERWATCH</li>
+              <li> FIFA</li>
+              <li> TEAM FIGHT TATICS</li>
+            </div>
+
+            {/*Texto informativo sobre os treinos*/}
+            <div className={styles.Treino}>
+              <h1>TREINOS</h1>
+              <hr/>
+              <pre className={styles.textTreino}>
+                {"Nossos treinos das modalidades físicas de futsal, vôlei e basquete, acontecem no Centro Poliesportivo do CEFET MG - Leopoldina.\n"}
+                {"Já nossos treinos das modalidades de E-sports acontecem de forma remota."}
+              </pre>
+            </div>  
+          </div>
+        </div>
+
+        {/*Breve texto para acessar o lintree dos treinos*/}
+        <div className={styles.AcesseTreino}>
+          <h1>TREINOS</h1>
+          <hr/>
+          <h2>Quer ser um atleta Panterão?</h2>
+        </div>
+
+        {/*Definição do botão do linktree*/}   
+        <div className={styles.AlinharBotao}>
+          <a href='https://linktr.ee/atleticacefetleopoldina?fbclid=PAAaaWLmmqzTtQD8Rd91MLRPsl2xphEkKPZEYuHeYrWGWES4P6NVKB0cZNvus'><button className={styles.Botao}>Saiba Mais</button></a>
+          {/* <button className={styles.Botao}>Saiba Mais</button> */}
+        </div>
+
+        {/*Alocação das fotos dos jogos*/}
+        
       </section>
     );
   }
